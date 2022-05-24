a50111a859c61d0f3f5d07016202749145b33ad7
Jocelin Nunes <jocelinnunes@gmail.com>
2022-05-24 15:54:25 -0300

    MongoDB auth created

--- routes/auth.js
+++ routes/auth.js
@@ -27,19 +27,23 @@ router.post("/login", async (req, res) => {
   try{
     
     const user = await User.findOne({username: req.body.username});
-    
+
     !user && res.status(401).json("Usuário não encontrado!")
 
     const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
 
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    password !== req.body.password &&
+    OriginalPassword !== req.body.password &&
       res.status(401).json("Senha incorreta!");
 
-      res.status(200).json(user);
+    const { password, ...others } = user._doc;
 
-  }catch(err){
+    //_doc é um diretorio do MongoDB
+
+    res.status(200).json(others);
+
+  } catch(err){
     res.status(500).json(err);
   }
 })
