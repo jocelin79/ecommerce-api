5d0e2a2072d3544b56c949f249f9539b9dd11e2c
Jocelin Nunes <jocelinnunes@gmail.com>
2022-06-06 12:02:41 -0300

    update sellers route

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const stripeRoute = require("./routes/stripe");
 dotenv.config()
 
 mongoose
-  .connect('mongodb+srv://jocelin_01:D1d2d3d4@cluster0.jsit9.mongodb.net/?retryWrites=true&w=majority')
+  .connect(process.env.MONGO_URL)
   .then(() => console.log('DB Connection Successfull'))
   .catch(err => {
     console.log(err)
--- routes/sellers.js
+++ routes/sellers.js
@@ -1,16 +1,37 @@
 const router = require("express").Router();
 const axios = require('axios');
 
-//GET SELLERS
+//GET SELLERSID
 
-router.get("/", async (req, res) => {
-  // const cep = req.params.cep
+router.get("/:cep", async (req, res) => {
+  const cep = req.params.cep
   var config = {
     method: 'get',
-    url: 'https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=14801788',
+    url: `https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=${cep}`,
     headers: { 
-      'X-VTEX-API-AppKey': 'vtexappkey-carrefourbr-PZLYPS', 
-      'X-VTEX-API-AppToken': 'UTARJONVBFAYJEOZGNAUZIRIFLMBNUVOOLWNTLIORNRCMGSIVEQFCTNNYOXYGHPUYEVLDHUVPKUHHBDOZPBKMOKGRITGDBXVQBDESIDQJWDANEZSIQVGSCZVEVPLCJVC'
+      'X-VTEX-API-AppKey': process.env.X_VTEX_API_AppKey_PCJ, 
+      'X-VTEX-API-AppToken': process.env.X_VTEX_API_AppToken_PCJ
+    }
+  };
+
+  await axios(config)
+  .then(function (response) {
+    res.status(200).json(response.data)
+  }) 
+  .catch(function (error) {
+    res.status(500).json(error);
+  });
+
+})
+
+router.get("/seller/:seller", async (req, res) => {
+  const seller = req.params.seller
+  var config = {
+    method: 'get',
+    url: `https://mercado.carrefour.com.br/api/catalog_system/pub/products/search?fq=${seller}`,
+    headers: { 
+      'X-VTEX-API-AppKey': process.env.X_VTEX_API_AppKey_PCJ, 
+      'X-VTEX-API-AppToken': process.env.X_VTEX_API_AppToken_PCJ
     }
   };
 
